74e5b51c2217de9cfb6b0d0a08c780a044d61ab3
MAlexGG <alexandragalarzag@gmail.com>
2025-04-23 21:51:06 +0200

    image service delete method without files removed because delete files from disk methods is used from model

--- src/main/java/com/newspaper/newspaper/service/ImageService.java
+++ src/main/java/com/newspaper/newspaper/service/ImageService.java
@@ -27,33 +27,20 @@ public class ImageService {
         Files.createDirectories(Paths.get("uploads/"));
         Path path = Paths.get("uploads/" + file.getOriginalFilename());
         Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-
         Image image = imageRepository.save(Image.builder()
             .name(file.getOriginalFilename())
             .path(path.toString())
             .build()
         );
-
         if (image != null) {
             return image;
         }
-        
         return null;
     }
 
     public void deleteImage(int imageId) {
-        Image image = getImage(imageId);
-        Path path = Paths.get(image.getPath());
-        try {
-            if (Files.exists(path)) {
-                Files.delete(path);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("No se pudo eliminar el archivo: " + path, e);
-        }
         imageRepository.deleteById(imageId); 
     }
-    
 
     public Image getImage(int id){
         Optional<Image> image = imageRepository.findById(id);
